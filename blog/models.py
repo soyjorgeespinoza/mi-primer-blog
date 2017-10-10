41b3c6c810a7f83845c8f3e7908c5d0e0d5f645a
Jorge Espinoza <esbajorge92@hotmail.com>
2017-10-10 08:01:21 -0600

    moidfique los templates que se muestran desde la BD

--- blog/models.py
+++ blog/models.py
@@ -3,10 +3,13 @@ from django.utils import timezone
 
 class Post(models.Model):
     autor = models.ForeignKey('auth.User')
-    titulo = models.CharField(max_length = 200)
+    titulo = models.CharField(
+        max_length = 200)
     texto = models.TextField()
-    fecha_creacion = models.DateTimeField(default = timezone.now)
-    fecha_publicacion = models.DateTimeField(blank = True, null = True)
+    fecha_creacion = models.DateTimeField(
+        default = timezone.now)
+    fecha_publicacion = models.DateTimeField(
+        blank = True, null = True)
 
 
     def publicar(self):
